aee4e38cc5bab40c2f6038a5238c64a93a4a861f
Samira-Hassann <152163512+Samira-Hassann@users.noreply.github.com>
2026-09-15 19:19:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.write("Upload an image to detect safety helmets and unprotected heads using *
 # Load model with caching
 @st.cache_resource
 def load_model():
-    return YOLO("best (1).pt")
+    return YOLO("best.pt")
 
 try:
     model = load_model()
